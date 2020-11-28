47c029e4b7c21c0e890701851429c03e71075ac8
Chukaidong <17302010007@fudan.edu.cn>
2020-11-28 17:07:27 +0800

    login

--- src/components/myHeader.vue
+++ src/components/myHeader.vue
@@ -5,36 +5,28 @@
         <a-menu v-model="current" mode="horizontal" style="text-align:right">
           <a-menu-item key="app">
             <a href="#/home">
-                <a-icon type="appstore" />9934小游戏
+              <a-icon type="appstore" />9934小游戏
             </a>
           </a-menu-item>
           <a-menu-item key="mail">
-            <a
-              href="mailto:17302010051@fudan.edu.cn"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href="mailto:17302010051@fudan.edu.cn" target="_blank" rel="noopener noreferrer">
               <a-icon type="mail" />联系我们
             </a>
           </a-menu-item>
           <a-menu-item key="github">
-            <a
-              href="https://github.com/whisIS/gameWeb"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <a-icon type="github" />加入我们</a
-            >
+            <a href="https://github.com/whisIS/gameWeb" target="_blank" rel="noopener noreferrer">
+              <a-icon type="github" />加入我们
+            </a>
           </a-menu-item>
         </a-menu>
       </a-col>
       <a-col :span="11">
         <div v-if="usrName == null">
           <a-menu v-model="current" mode="horizontal" style="text-align:left">
-            <a-menu-item key="login">
+            <a-menu-item key="login" @click="login">
               <a-icon type="login" />登录
             </a-menu-item>
-            <a-menu-item key="signup">
+            <a-menu-item key="signup" @click="sign">
               <a-icon type="user-add" />注册
             </a-menu-item>
           </a-menu>
@@ -61,6 +53,10 @@
         </div>
       </a-col>
     </a-row>
+    <a-modal :title="title" :visible="visible" :confirm-loading="confirmLoading" @cancel="handleCancel" :footer="null">
+      <login v-if="title == '登录'"/>
+      <sign v-if="title == '注册'"/>
+    </a-modal>
   </div>
 </template>
 
@@ -69,8 +65,26 @@ export default {
   name: "myHeader",
   data() {
     return {
-      usrName: "yanghui",
+      current: ['mail'],
+      usrName: null,
+      title: "登录",
+      visible: false,
+      confirmLoading: false
     };
   },
+  methods: {
+    login(){
+      this.title="登录";
+      this.visible = true;
+    },
+    sign(){
+      this.title="注册";
+      this.visible = true;
+    },
+    handleCancel(){
+      this.visible=false;
+    }
+
+  }
 };
 </script>
